a5874be015a3d02a8e8a53b0445b3a06c2c30468
belongix <teambelongix@gmail.com>
2026-08-26 21:51:04 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.BELONGIX_CONFIG={
-  SUPABASE_URL:'https://efhcfuaxgbzuqlmhlsxc.supabase.co',
-  SUPABASE_ANON_KEY:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmaGNmdWF4Z2J6dXFsbWhsc3hjIiwiaWF0IjoxNzc3MTQ4NTc4LCJleHAiOjIwOTI3MjQ1Nzh9.vpFvBPnKkrMMONXo9z6FemJ2qIlRChRloQYRB0LMdjY',
-  AI_FUNCTION_URL:'',
-  APP_NAME:'Belongix'
-};
