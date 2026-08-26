c5a01dd232ffe72b0287ed906c6e80b66db2db28
belongix <teambelongix@gmail.com>
2026-08-26 22:18:21 +0530

    Update Supabase anon key in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.BELONGIX_CONFIG = {
   SUPABASE_URL: 'https://vtiaszkqpsuuvfaizqrl.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJIUzI1NiIsInJlZiI6InZ0aWFzemtxcHN1dXZmYWl6cXJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc1ODcwNDcsImV4cCI6MjEwMzE2MzA0N30.LUbkySSgblig5C13C2ROhHr1--nZraJWhw0Rji7ZdUE',
+  SUPABASE_ANON_KEY: 'sb_publishable_o2OsxbEzbVd8JXdVU_z7Zw_kpmzt-nm',
   RESUME_AI_FUNCTION_URL: 'https://vtiaszkqpsuuvfaizqrl.supabase.co/functions/v1/resume-ai',
   APP_NAME: 'Belongix'
 };
